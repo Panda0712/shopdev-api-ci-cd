aff0193a1344f7ade81f67240e21b6d5de7e3238
Pham Tuan <tuan.pham.super@gmail.com>
2025-07-24 13:43:19 +0700

    add api router

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+    data: [
+      {
+        name: "messi",
+        age: 40,
+      },
+    ],
+  });
+});
 
 module.exports = router;
